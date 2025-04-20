822d43ea8ad7ea8f33b9296aa1922c2ed64cb20f
MubarkSalimQoins <mubarkqains@gmail.com>
2025-04-20 23:19:47 +0300

    رفع ملفات جديده وتحديث المشروع

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,114 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screens/categories_screns.dart';
-import 'app_data.dart';
+import './app_data.dart';
+import 'package:traveling_app/screens/categories_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import './screens/filters_screen.dart';
+import './screens/tabs_screen.dart';
+import './screens/trip_detail_screen.dart';
+import './screens/category_trips_screen.dart';
+import './models/trip.dart';
 
 void main() {
-  runApp(myapp());
+  runApp(MyApp());
 }
 
-class myapp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'summer': false,
+    'winter': false,
+    'family': false,
+  };
+
+  List<Trip> _availableTrips = Trips_data;
+  List<Trip> _favoriteTrips = [];
+
+  void _changeFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+
+      _availableTrips = Trips_data.where((trip) {
+        if (_filters['summer'] == true && trip.isInSummer != true) {
+          return false;
+        }
+        if (_filters['winter'] == true && trip.isInWinter != true) {
+          return false;
+        }
+        if (_filters['family'] == true && trip.isForFamilies != true) {
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+
+  void _manageFavorite(String tripId) {
+    final existingIndex =
+        _favoriteTrips.indexWhere((trip) => trip.id == tripId);
+    if (existingIndex >= 0) {
+      setState(() {
+        _favoriteTrips.removeAt(existingIndex);
+      });
+    } else {
+      setState(() {
+        _favoriteTrips.add(
+          Trips_data.firstWhere((trip) => trip.id == tripId),
+        );
+      });
+    }
+  }
+
+  bool _isFovarite(String id) {
+    return _favoriteTrips.any((trip) => trip.id == id);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "travel",
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('ar', 'AE'), // Arabic locale
+      ],
+      title: 'Travel App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Colors.amber, // Accent color replaced by secondary color
+        ),
+        fontFamily: 'ElMessiri',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headlineLarge: TextStyle(
+                color: Colors.blue,
+                fontSize: 24,
+                fontFamily: 'ElMessiri',
+                fontWeight: FontWeight.bold,
+              ),
+              headlineMedium: TextStyle(
+                color: Colors.white,
+                fontSize: 26,
+                fontFamily: 'ElMessiri',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
       ),
-      home: CategoriesScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (ctx) => TabsScreen(_favoriteTrips),
+        CategoryTripsScreen.screenRoute: (ctx) =>
+            CategoryTripsScreen(_availableTrips),
+        TripDetailScreen.screenRoute: (ctx) =>
+            TripDetailScreen(_manageFavorite, _isFovarite),
+        FiltersScreen.screenRoute: (ctx) =>
+            FiltersScreen(_filters, _changeFilters),
+      },
     );
   }
 }
